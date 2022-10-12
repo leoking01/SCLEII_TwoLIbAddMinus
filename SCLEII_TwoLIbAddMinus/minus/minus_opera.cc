8dfb2ad1dbf3efde758699bb49fb7ee08d4ce5df
leo <leoking02@foxmail.com>
2022-10-12 09:05:12 +0800

    first commit 222

---
+++ SCLEII_TwoLIbAddMinus/minus/minus_opera.cc
@@ -0,0 +1,9 @@
+#include "minus_opera.h"
+
+int minus(int a, int b){
+    return a-b;
+}
+
+double minus(double a, double b){
+    return a-b;
+}
---
+++ SCLEII_TwoLIbAddMinus_19.33.31630.0_install/include/minus_opera.h
@@ -0,0 +1,6 @@
+#ifndef MYMATH_H
+#define MYMATH_H
+
+int minus(int, int);
+double minus(double, double);
+#endif
